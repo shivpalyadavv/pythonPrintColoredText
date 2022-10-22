6ed850e7935441b31f948e2bacfa1f0ceaa7d47a
shivpalyadavv <shivpaly2@gmail.com>
2022-10-22 22:38:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Shival "+ Fore.YELLOW+ Back.BLUE+"good boy, Bad boy!!")
+print(Back.CYAN+"Hi My name is Shipval")
+print(Fore.RED + Back.GREEN+ "Hi My name is Shivpal")
